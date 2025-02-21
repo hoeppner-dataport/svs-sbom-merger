71d164e0a45f2c3cc5a81790674412db4ce7655f
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-02-21 11:46:45 +0100

    u4

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  core.log(`Hello ${nameToGreet}!`);
+  core.info(`Hello ${nameToGreet}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context?.payload, undefined, 2);
-  core.log(`The event payload: ${payload}`);
+  core.info(`The event payload: ${payload}`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 } catch (error) {
